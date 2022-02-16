2317867e7825b7c1246a7815b6005473663050a4
kanzubae <42201418+kanzubae@users.noreply.github.com>
2022-02-16 10:57:19 +0700

    Update and rename 300x250 to 300x250.js

--- 300x250.js
+++ 300x250.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
 	atOptions = {
 		'key' : '0b914f53bdd00964e4fb9fb1b2989606',
 		'format' : 'iframe',
@@ -7,4 +6,4 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/0b914f53bdd00964e4fb9fb1b2989606/invoke.js"></scr' + 'ipt>');
-</script>
+
